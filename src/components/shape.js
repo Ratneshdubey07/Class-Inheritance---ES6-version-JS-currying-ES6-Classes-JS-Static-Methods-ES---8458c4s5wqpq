0f76c7f81baab7e25d76cf5b611361502b902147
Ratneshdubey07 <111305768+Ratneshdubey07@users.noreply.github.com>
2022-12-09 21:16:34 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,13 @@
-// declare class
-
-// export class using module.exports
+class Shape {
+    constructor() {
+      this.color = "green";
+    }
+    drawShape() {
+      
+    }
+    calculateArea() {
+      
+    }
+  }
+  // export class using module.exports
+    module.exports = Shape;
